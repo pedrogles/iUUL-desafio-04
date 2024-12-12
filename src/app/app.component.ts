3638ae4d54099452f226ebf7c65911e83dd09475
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-12-12 13:20:19 -0300

    feat(footer): criação do rodapé da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layouts/header/header.component";
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./layouts/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
