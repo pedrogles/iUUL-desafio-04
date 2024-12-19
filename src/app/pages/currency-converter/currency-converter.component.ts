bd6c8df5e744b70766326dd91e739a2a49831314
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-12-19 18:28:55 -0300

    feat(conversão de moedas): adição dos metodos para armazenamento das conversões em localStorage

--- src/app/pages/currency-converter/currency-converter.component.ts
+++ src/app/pages/currency-converter/currency-converter.component.ts
@@ -5,6 +5,8 @@ import { SupportedCodesService } from '../../shared/services/supported-codes/sup
 import { Coin } from '../../shared/models/supported-codes.model';
 import { PairConversionService } from '../../shared/services/pair-conversion/pair-conversion.service';
 import { ConversionData } from '../../shared/models/pair-conversion.model';
+import { LocalStorageService } from '../../shared/services/local-storage/local-storage.service';
+import { Conversion } from '../../shared/models/local-storage.model';
 
 @Component({
   selector: 'app-currency-converter',
@@ -18,6 +20,8 @@ export class CurrencyConverterComponent {
   resultContainer: boolean = false;
   converterForm!: FormGroup;
   coins!: Coin[];
+  date = new Date();
+  isHighValue!: boolean;
   conversionResult: ConversionData = {
     from: undefined,
     to: undefined,
@@ -28,7 +32,8 @@ export class CurrencyConverterComponent {
 
   constructor(
       private supportedCodesService: SupportedCodesService, 
-      private pairConversionService: PairConversionService
+      private pairConversionService: PairConversionService,
+      private localStorageService: LocalStorageService
   ) {}
 
   ngOnInit() {
@@ -66,10 +71,73 @@ export class CurrencyConverterComponent {
             conversion_result: response.conversion_result
           }
           this.resultContainer = true;
+          this.checkAndStoreConversion();
         },
         error: (err) => console.error('Erro ao realizar conversão:', err.message)
       });
     }
+  }
+
+  private checkAndStoreConversion() {
+    const conversionData = {
+      from: this.conversionResult.to,
+      to: 'USD',
+      value: this.conversionResult.conversion_result
+    }
+
+    this.pairConversionService.getPairConversion(conversionData).subscribe({
+      next: (response) => {
+        response.conversion_result! > 1000 ? this.isHighValue = true : this.isHighValue = false
+        this.updateConversionHistory()
+      },
+      error: (err) => console.error('Erro ao realizar conversão:', err.message)
+    });
+  }
+
+  private updateConversionHistory() {
+    if(this.localStorageService.getItem('conversion-history')){
+      const conversionHistory: Conversion[] | null = this.localStorageService.getItem('conversion-history')
+      const lastId = conversionHistory![conversionHistory!.length - 1].id
+
+      const newConversion = {
+          id: lastId + 1,
+          conversion_date: this.getFormatedDate(this.date), 
+          conversion_time: this.getFormatedHour(this.date), 
+          from_currency: this.conversionResult.from!, 
+          to_currency: this.conversionResult.to!, 
+          input_value: this.conversionResult.value!, 
+          output_value: this.conversionResult.conversion_result!, 
+          exchange_rate: this.conversionResult.conversion_rate!, 
+          isHigh_value:  this.isHighValue
+      };
+
+      conversionHistory!.push(newConversion);
+      this.localStorageService.setItem('conversion-history', conversionHistory)
+    } 
+    else {
+      const firstConversion = {
+        id: 1,
+        conversion_date: this.getFormatedDate(this.date), 
+        conversion_time: this.getFormatedHour(this.date), 
+        from_currency: this.conversionResult.from!, 
+        to_currency: this.conversionResult.to!, 
+        input_value: this.conversionResult.value!, 
+        output_value: this.conversionResult.conversion_result!, 
+        exchange_rate: this.conversionResult.conversion_rate!, 
+        isHigh_value: this.isHighValue 
+      }
+
+      const conversionHistory = [firstConversion]
+      this.localStorageService.setItem('conversion-history', conversionHistory)
+    }
+  }
+
+  private getFormatedDate(date: Date): string {
+    return `${date.getDate().toString().padStart(2, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear()}`
+  }
+
+  private getFormatedHour(date: Date): string {
+    return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
   }
 
   handleInvertCoins() {
